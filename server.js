eb615cbe875543b3efbc7bd6f73311b52a1378be
beaconb <beaconb.mail@gmail.com>
2016-06-13 17:26:39 +0200

    DB

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-var port  	 = process.env.PORT || 7777;
+var port  	 = process.env.PORT || 80;
 
 //*********************************** ESTABLECEMOS LA CONEXION A LA BB.DD. ************************************************************
 
